8a43370c0e2e37a97a3a011417faca1e929e0c75
Failesmen <failesmenfalw@gmail.com>
2021-10-13 22:01:32 -0300

    all files have been fixed

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView showsHorizontalScrollIndicator={false} style={styles.container}>
+    <Text>Sardonyx Animais</Text>
+    
+      <Text>Alce</Text>
+      <Image source={require('./assets/Alce.jpg')} style={styles.mamiferos}/>
+      
+      <Text>Baleia Azul</Text>
+      <Image source={require('./assets/Baleia_azul.jpg')} style={styles.mamiferos}/>
+
+      <Text>Coala</Text>
+      <Image source={require('./assets/coala.jpg')} style={styles.mamiferos}/>
+
+      <Text>Doninha</Text>
+      <Image source={require('./assets/Doninha.jpg')} style={styles.mamiferos}/>
+
+      <Text>Esquilo</Text>
+      <Image source={require('./assets/Esquilo.jpg')} style={styles.mamiferos}/>
+
+      <Text>Gambá</Text>
+      <Image source={require('./assets/Gambá.jpg')} style={styles.mamiferos}/>
+
+      <Text>Panda</Text>
+      <Image source={require('./assets/Panda.jpg')} style={styles.mamiferos}/>
+
+    </ScrollView>
   );
 }
 
@@ -15,7 +36,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+
+  mamiferos: {
+    width: '100%',
+    height: 400,
+    marginBottom: 10
+  }
 });
